e8a95a2a4710d616466ed273f883d399bfc1141e
Krishna <krishnait06@gmail.com>
2024-01-06 10:50:10 +0530

    role page association changes updated

--- src/app/+admin/components/role-page-associate/role-page-associate.component.ts
+++ src/app/+admin/components/role-page-associate/role-page-associate.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TreeNode } from 'primeng/api';
+import { NodeService } from 'src/app/demo/service/node.service';
 
 @Component({
   selector: 'app-role-page-associate',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./role-page-associate.component.scss']
 })
 export class RolePageAssociateComponent {
+  files1: TreeNode[] = [];
+  selectedFiles1: TreeNode<any> | TreeNode<any>[] | any[] | any;
 
+
+  constructor(private nodeService: NodeService) { }
+
+  ngOnInit() {
+    this.nodeService.getFiles().then(files => this.files1 = files);
+  }
+
+  Save() {
+
+  }
+
+  Clear() {
+
+  }
 }
--- src/app/+admin/components/role-page-associate/role-page.module.ts
+++ src/app/+admin/components/role-page-associate/role-page.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { RolePageRoutingModule } from './role-page-routing.module';
 import { RolePageAssociateComponent } from './role-page-associate.component';
+import { TreeModule } from 'primeng/tree';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
 
 
 @NgModule({
@@ -11,7 +16,12 @@ import { RolePageAssociateComponent } from './role-page-associate.component';
   ],
   imports: [
     CommonModule,
-    RolePageRoutingModule
+    RolePageRoutingModule,
+
+    ReactiveFormsModule,
+    ButtonModule, RippleModule,
+    ToolbarModule,
+    TreeModule,
   ]
 })
 export class RolePageModule { }
